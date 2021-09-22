93bcb663b9b623afbe526169552149a0f064a1cd
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2021-09-22 20:08:13 +1000

    main: navbar cleanup name change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../Button';
 import MenuItems from './MenuItems';
@@ -6,8 +5,6 @@ import MenuItems from './MenuItems';
 import './Navbar.css';
 
 const Navbar = () => {
-  const [menuIconClicked, setMenuIconClicked] = useState(false);
-
   const menuItemsMapped = MenuItems.map((item) => (
     <li>
       <Link key={item.title} className={item.cName} to={item.url}>
@@ -16,26 +13,13 @@ const Navbar = () => {
     </li>
   ));
 
-  const onMenuIconClickHandler = () => {
-    setMenuIconClicked(!menuIconClicked);
-  };
-
   return (
     <nav className="NavbarItems">
       <h1 className="navbar-logo">
-        React
+        GitBlog
         <i className="fab fa-react" />
       </h1>
-      <div
-        className="menu-icon"
-        onClick={onMenuIconClickHandler}
-        onKeyDown={onMenuIconClickHandler}
-        role="menuitem"
-        tabIndex={0}
-      >
-        <i className={menuIconClicked ? 'fas fa-times' : 'fas fa-bars'} />
-      </div>
-      <ul className={menuIconClicked ? 'nav-menu active' : 'nav-menu'}>
+      <ul className="nav-menu">
         {menuItemsMapped}
       </ul>
       <Button>Sign Up</Button>
